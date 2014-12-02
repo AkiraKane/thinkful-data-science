e8b91eee9d6e9c31f88f9398e648f3b0451cc19a
Kevin Keller <flygeneticist@users.noreply.github.com>
2014-12-02 15:42:12 -0500

    Code review
    Comments and modifications to pull together the rural and urban values for the total density of the continent.

--- Unit 1/U01-L01-C05-challenge2-pop_land_area.py
+++ Unit 1/U01-L01-C05-challenge2-pop_land_area.py
@@ -5,21 +5,36 @@
 from collections import defaultdict
 import csv
 
-population_dict = defaultdict(dict)
+population_dict = {} # simple plain dictionary
 
-with open('lecz-urban-rural-population-land-area-estimates-v2-csv/lecz-urban-rural-population-land-area-estimates_continent-90m.csv','rU') as inputFile:
-	inputReader = csv.reader(inputFile)
-	header = next(inputFile)
-	for line in inputReader:
-		continent = str(line[0])
-		urbanrural = str(line[2])
-		pop2010 = int(line[5])
-		landarea = float(line[7])
-		if line[1] == 'Total National Population':
-			population_dict[continent][urbanrural] = pop2010/landarea
-	
-with open('U01-L01-C05-challenge2-pop_land_area.csv', 'w') as outputFile:
-	outputFile.write('continent, urbanrural, density\n')
-	for k, v in population_dict.iteritems():
-		for c in v.iteritems():
-			outputFile.write(str(k) + ',' + str(c[0]) + ',' + str(c[1]) + '\n')
+with open('lecz-urban-rural-population-land-area-estimates_continent-90m.csv','rU') as inputFile:
+  inputReader = csv.reader(inputFile) # good use of the csv_reader! Makes life a lot easier. 
+  header = next(inputFile) 
+  
+  for line in inputReader:
+    continent = line[0] # no need to convert to a string, as it is already in that format.
+    urbanrural = line[2] # same with the rural / urban
+    pop2010 = int(line[5])
+    landarea = float(line[7])
+  
+    if line[1] == 'Total National Population':
+      # tests to see if the continent is not already a key value the population_dict
+      if line[0] not in population_dict: 
+        # create the continent key and link it to the defaultdict  
+        population_dict[line[0]] = defaultdict(int) 
+      # now that we know the key exists with the defaultdict, add the pop & area
+      population_dict[continent]['pop'] += pop2010 
+      population_dict[continent]['area'] += landarea
+
+# print out the results
+for k, v in population_dict.iteritems():
+  print "Continent: {}\nPop: {}\nArea: {}\nDensity:{}\n".format(k, v['pop'], v['area'], round(v['pop']/v['area'],2))
+
+# if any of the above code doesn't make sense, you find a bug in my code/logic, or you think of a different way to approach it email me! 
+
+# I'll leave it to you to re-write the file output part with the nested new dictionary. :)
+# with open('U01-L01-C05-challenge2-pop_land_area.csv', 'w') as outputFile:
+#   outputFile.write('continent, urbanrural, density\n')
+#   for k, v in population_dict.iteritems():
+#     for c in v.iteritems():
+#       outputFile.write(str(k) + ',' + str(c[0]) + ',' + str(c[1]) + '\n')
