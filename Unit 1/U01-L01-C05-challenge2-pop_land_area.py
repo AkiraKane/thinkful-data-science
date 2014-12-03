eb0ac4a1240d1c86627d6e87a888f589ec9c71ca
Denis Mittakarin <dmittakarin8@hotmail.com>
2014-12-02 20:01:28 -0500

    Changed [line[0]] references to [continent]

--- Unit 1/U01-L01-C05-challenge2-pop_land_area.py
+++ Unit 1/U01-L01-C05-challenge2-pop_land_area.py
@@ -7,7 +7,7 @@ import csv
 
 population_dict = {} # simple plain dictionary
 
-with open('lecz-urban-rural-population-land-area-estimates_continent-90m.csv','rU') as inputFile:
+with open('lecz-urban-rural-population-land-area-estimates-v2-csv/lecz-urban-rural-population-land-area-estimates_continent-90m.csv','rU') as inputFile:
   inputReader = csv.reader(inputFile) # good use of the csv_reader! Makes life a lot easier. 
   header = next(inputFile) 
   
@@ -19,9 +19,9 @@ with open('lecz-urban-rural-population-land-area-estimates_continent-90m.csv','r
   
     if line[1] == 'Total National Population':
       # tests to see if the continent is not already a key value the population_dict
-      if line[0] not in population_dict: 
+      if continent not in population_dict: 
         # create the continent key and link it to the defaultdict  
-        population_dict[line[0]] = defaultdict(int) 
+        population_dict[continent] = defaultdict(int) 
       # now that we know the key exists with the defaultdict, add the pop & area
       population_dict[continent]['pop'] += pop2010 
       population_dict[continent]['area'] += landarea
